89b03e4469c760285611707f1edd000a36a34cf1
Samuel Elliott <sam@lenary.co.uk>
2008-09-01 01:17:07 +0100

    added migration

--- db/migrate/20080831232047_create_stats.rb
+++ db/migrate/20080831232047_create_stats.rb
@@ -1,8 +1,22 @@
 class CreateStats < ActiveRecord::Migration
   def self.up
     create_table :stats do |t|
+      t.string :session_id, :limit => 32 # Session ID
+      t.string :path # Requested Path
+      t.string :method # Requested Method, eg "AccountController#login"
+      t.string :params # Request Paramaters, stored as a hash
 
-      t.timestamps
+      t.string :for # IP Address
+      
+      t.float :total # Total Process Time, Seconds
+      t.float :render # Render Time, Seconds
+      t.float :db # Database Time, Seconds
+      
+      t.integer :status # HTTP Response Code
+      
+      t.time :at # Request Time
+
+      t.timestamps # created_at and updated_at
     end
   end
 
